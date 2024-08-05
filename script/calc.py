a1f36c8378b7171633b662ed421484ba3e173339
Natacha M <114677507+NataTCHA@users.noreply.github.com>
2024-08-05 10:24:02 +0200

    ajout_f0,amp_leonardo

--- script/calc.py
+++ script/calc.py
@@ -14,8 +14,9 @@ from scipy.signal import butter, filtfilt, hilbert
 from scipy.interpolate import interp1d
 from scipy import interpolate
 import copy
+
 def applyFilter(
-                x,
+                 x:npt.NDArray,
                 sr,
                 /,*,
                 filt:str='iir',
@@ -66,7 +67,7 @@ def applyFilter(
     """
     # if len(cutOff)==1:
     #     cutOff=cutOff[0]
-        
+    y = None
     if (filt is None) | (cutOff is None) | (cutOff is None):
         if cutOff is None:
             raise Exception('Cannot apply filter without specifying a cut Off freq. (CutOff is None).')
@@ -122,23 +123,45 @@ def applyFilter(
     
     return y
 
-def calc_formants(sound: parselmouth.Sound, start_time: float, end_time: float, energy_threshold: float = 20.0):
-    formants = sound.to_formant_burg(time_step=0.005, max_number_of_formants=5, maximum_formant=5500, window_length=0.025, pre_emphasis_from=50)
+def calc_formants(
+    sound: parselmouth.Sound,
+    start_time: float,
+    end_time: float,
+    energy_threshold: float = 20.0,
+    time_step: float = 0.005,
+    max_number_of_formants: int = 5,
+    maximum_formant: float = 5500.0,
+    window_length: float = 0.025,
+    pre_emphasis_from: float = 50.0
+):
+    formants = sound.to_formant_burg(
+        time_step=time_step,
+        max_number_of_formants=max_number_of_formants,
+        maximum_formant=maximum_formant,
+        window_length=window_length,
+        pre_emphasis_from=pre_emphasis_from
+    )
     time_values = formants.ts()
-    
-    formant_values = {i: {time: formants.get_value_at_time(formant_number=i, time=time) for time in time_values if start_time <= time <= end_time} for i in range(1, 4)}
-    
+
+    formant_values = {
+        i: {time: formants.get_value_at_time(formant_number=i, time=time) for time in time_values if start_time <= time <= end_time}
+        for i in range(1, 4)
+    }
+
     intensities = sound.to_intensity()
     frame_energies = {time: intensities.get_value(time) for time in time_values}
-    
+
     filtered_formant_values = {
         i: {time: value for time, value in formant_values[i].items() if frame_energies.get(time, 0) > energy_threshold}
         for i in range(1, 4)
     }
-    
+
     time_values_filtered = sorted(filtered_formant_values[1].keys())
-    resampled_formants = {i: np.array([filtered_formant_values[i][time] for time in time_values_filtered]) for i in range(1, 4)}
-    
+    resampled_formants = {
+        i: np.array([filtered_formant_values[i][time] for time in time_values_filtered])
+        for i in range(1, 4)
+    }
+
     return time_values_filtered, resampled_formants[1], resampled_formants[2], resampled_formants[3]
 def read_AG50x(path_to_pos_file):
     dims = ["x", "z", "y", "phi", "theta", "rms", "extra"]
@@ -312,44 +335,6 @@ def calculate_amplitude_envelope(
     
     return amp, ampT
 
-class MinMaxFinder:
-    def find_in_interval(self, times: list[float], values: list[float], interval: tuple[float, float]) -> tuple[np.ndarray[float], np.ndarray[float]]:
-        start, end = interval
-        interval_times = []
-        interval_values = []
-        for time, value in zip(times, values):
-            in_interval: bool = start <= time and time <= end
-            if not in_interval:
-                continue
-            interval_times.append(time)
-            interval_values.append(value)
-        return np.array(interval_times), np.array(interval_values)
-
-    def analyse_minimum(self, x, y, interval):
-        if interval is None:
-            print("No interval specified.")
-            return [], []
-        interval_times, interval_values = self.find_in_interval(x, y, interval)
-        min_peaks, _ = scipy.signal.find_peaks(-interval_values)
-        if len(min_peaks) == 0:
-            return [], []
-        min_times = interval_times[min_peaks]
-        min_values = interval_values[min_peaks]
-        return min_times, min_values
-
-    def analyse_maximum(self, x, y, interval):
-        if interval is None:
-            print("No interval specified.")
-            return [], []
-        interval_times, interval_values = self.find_in_interval(x, y, interval)
-        max_peaks, _ = scipy.signal.find_peaks(interval_values)
-        if len(max_peaks) == 0:
-            return [], []
-        max_times = interval_times[max_peaks]
-        max_values = interval_values[max_peaks]
-        return max_times, max_values
-
-
 def interp_NAN(
         X:npt.NDArray, 
         method:str='linear'
@@ -391,8 +376,8 @@ def interp_NAN(
 
     newX[mynans] = justnans[mynans]
     return newX
-def get_f0(x,
-           sr,
+def get_f0(x:npt.NDArray,
+           sr:float,
            method:str='praatac', 
            hopSize:float=0.01,
            minPitch:float=75,
@@ -598,3 +583,40 @@ def get_f0(x,
         f0=applyFilter(f0,1/hopSize,filt=outFilter,cutOff=outFiltCutOff, filtLen=outFiltLen, filtType=outFiltType,polyOrd=outFiltPolyOrd)
     
     return f0,f0t
+
+class MinMaxFinder:
+    def find_in_interval(self, times: list[float], values: list[float], interval: tuple[float, float]) -> tuple[np.ndarray[float], np.ndarray[float]]:
+        start, end = interval
+        interval_times = []
+        interval_values = []
+        for time, value in zip(times, values):
+            in_interval: bool = start <= time and time <= end
+            if not in_interval:
+                continue
+            interval_times.append(time)
+            interval_values.append(value)
+        return np.array(interval_times), np.array(interval_values)
+
+    def analyse_minimum(self, x, y, interval):
+        if interval is None:
+            print("No interval specified.")
+            return [], []
+        interval_times, interval_values = self.find_in_interval(x, y, interval)
+        min_peaks, _ = scipy.signal.find_peaks(-interval_values)
+        if len(min_peaks) == 0:
+            return [], []
+        min_times = interval_times[min_peaks]
+        min_values = interval_values[min_peaks]
+        return min_times, min_values
+
+    def analyse_maximum(self, x, y, interval):
+        if interval is None:
+            print("No interval specified.")
+            return [], []
+        interval_times, interval_values = self.find_in_interval(x, y, interval)
+        max_peaks, _ = scipy.signal.find_peaks(interval_values)
+        if len(max_peaks) == 0:
+            return [], []
+        max_times = interval_times[max_peaks]
+        max_values = interval_values[max_peaks]
+        return max_times, max_values
